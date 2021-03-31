f002cadccf8867ab5b9e85b7d7a9ecf25c6d8a83
Chavirax <71085426+Chavirax@users.noreply.github.com>
2021-03-31 08:16:01 -0600

    first commit

---
+++ Components/TeamTypeViewComponents.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Assignment10.Models;
+
+namespace Assignment10.Components
+{
+    public class TeamTypeViewComponents : ViewComponent
+    {
+        private BowlingLeagueContext context;
+
+        public TeamTypeViewComponents(BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x));
+        }
+
+    }
+}
